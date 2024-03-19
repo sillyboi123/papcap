b17d7a0ef9e8fcd4870c01672129f622e41cb0d4
sillyboi123 <163807951+sillyboi123@users.noreply.github.com>
2024-03-19 05:19:58 +0000

    Update papcap-install.sh

--- papcap-install.sh
+++ papcap-install.sh
@@ -8,7 +8,7 @@ touch /usr/local/sbin/rp-pppoe-server.sh
 echo '#!/bin/bash' >> /usr/local/sbin/rp-pppoe-server.sh
 echo 'modprobe pppoe' >> /usr/local/sbin/rp-pppoe-server.sh
 echo '/usr/sbin/pppoe-server -k -C Server -I eth0' >> /usr/local/sbin/rp-pppoe-server.sh
-echo 'echo "THE PID OF THIS SCRIPT TO KILL LATER IS BELOW, THE FIRST LINE SECOND SEQUENCE OF NUMBERS BEFORE "/usr/local/sbin/rp-pppoe-server.sh" STARTING FROM THE LEFT TO THAT PATH, USE kill PIDNUMBER TO FINISH" ' >> /usr/local/sbin/rp-pppoe-server.sh
+echo 'echo "THE PID OF THIS SCRIPT TO KILL LATER IS BELOW, BEFORE DOING THIS MAKE SURE THAT YOU TURNED OFF THE ROUTER, THE FIRST LINE SECOND SEQUENCE OF NUMBERS BEFORE "/usr/local/sbin/rp-pppoe-server.sh" STARTING FROM THE LEFT TO THAT PATH, USE kill PIDNUMBER TO FINISH" ' >> /usr/local/sbin/rp-pppoe-server.sh
 echo 'ps -ef | grep rp-pppoe-server.sh' >> /usr/local/sbin/rp-pppoe-server.sh
 chmod a+x /usr/local/sbin/rp-pppoe-server.sh
 echo 'BEFORE STARTING THE SERVER MAKE SURE THAT THE ROUTER IS OFF AND CONNECT THE CABLE LEAVING YOUR ETH0 PORT AND CONNECT TO THE WAN PORT OF ROUTER, NOW OPEN WIRESHARK AS ROOT USING wireshark -i eth0 -k -S , THEN START THE SERVER USING /usr/local/sbin/rp-pppoe-server.sh , GO BACK TO THE WIRESHARK AND USING THE FILTER TOOLBAR SELECT THE "pap" FILTER AND START CAPTURING PAP PACKETS'
