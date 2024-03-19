76d00f3a760d9f28380ba8c714640e28faf4d6d4
sillyboi123 <163807951+sillyboi123@users.noreply.github.com>
2024-03-19 05:05:40 +0000

    Update papcap-install.sh

--- papcap-install.sh
+++ papcap-install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sudo apt update
-sudo apt install make gcc ppp ppp-dev iptables wireshark
+apt update
+apt install make gcc ppp ppp-dev iptables wireshark
 git clone https://github.com/dfskoll/rp-pppoe.git
-cd rp-pppoe && cd src && sudo ./configure –enable-plugin && sudo make && sudo make install
+cd rp-pppoe && cd src && ./configure --enable-plugin
+echo 'You will need to type "cd rp-pppoe" press enter, "cd src" press enter, "make" press enter and "make install" press enter as root to install the rp-pppoe'
 touch /usr/local/sbin/rp-pppoe-server.sh
 echo '#!/bin/bash' >> /usr/local/sbin/rp-pppoe-server.sh
 echo 'modprobe pppoe' >> /usr/local/sbin/rp-pppoe-server.sh
 echo '/usr/sbin/pppoe-server -k -C Server -I eth0' >> /usr/local/sbin/rp-pppoe-server.sh
-echo 'echo "THE PID OF THIS SCRIPT TO KILL LATER IS BELOW, THE FIRST LINE SECOND SEQUENCE OF NUMBERS BEFORE "sudo /usr/local/sbin/rp-pppoe-server.sh", USE kill PIDNUMBER TO FINISH" ' >> /usr/local/sbin/rp-pppoe-server.sh
+echo 'echo "THE PID OF THIS SCRIPT TO KILL LATER IS BELOW, THE FIRST LINE SECOND SEQUENCE OF NUMBERS BEFORE "/usr/local/sbin/rp-pppoe-server.sh" STARTING FROM THE LEFT TO THAT PATH, USE kill PIDNUMBER TO FINISH" ' >> /usr/local/sbin/rp-pppoe-server.sh
 echo 'ps -ef | grep rp-pppoe-server.sh' >> /usr/local/sbin/rp-pppoe-server.sh
-sudo chmod a+x /usr/local/sbin/rp-pppoe-server.sh
-sudo wireshark -i eth0 -k -S
+chmod a+x /usr/local/sbin/rp-pppoe-server.sh
+echo 'BEFORE STARTING THE SERVER MAKE SURE THAT THE ROUTER IS OFF AND CONNECT THE CABLE LEAVING YOUR ETH0 PORT AND CONNECT TO THE WAN PORT OF ROUTER, NOW OPEN WIRESHARK AS ROOT USING wireshark -i eth0 -k -S , THEN START THE SERVER USING /usr/local/sbin/rp-pppoe-server.sh , GO BACK TO THE WIRESHARK AND USING THE FILTER TOOLBAR SELECT THE "pap" FILTER AND START CAPTURING PAP PACKETS'
