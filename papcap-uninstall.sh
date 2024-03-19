15dc146d41f3b545fedc8da6879d7400ddf661e5
sillyboi123 <163807951+sillyboi123@users.noreply.github.com>
2024-03-19 04:24:55 +0000

    Create papcap-uninstall.sh

---
+++ papcap-uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo rm -rf /etc/ppp/
+sudo rm -rf /usr/share/doc/rp-pppoe-4.0/
+sudo rm -rf /usr/sbin/pppoe-relay
+sudo rm -rf /usr/sbin/pppoe-sniff
+sudo rm -rf /usr/sbin/pppoe-server
+sudo rm -rf /usr/sbin/pppoe
+sudo rm /usr/share/man/man8/pppoe.8
+sudo rm /usr/share/man/man8/pppoe-server.8
+sudo rm /usr/share/man/man8/pppoe-sniff.8
+sudo rm /usr/share/man/man8/pppoe-relay.8
+sudo rm /usr/local/sbin/rp-pppoe-server.sh
