054f3380f10d78970a0df0b831dc9b5158022bf8
sillyboi123 <163807951+sillyboi123@users.noreply.github.com>
2024-03-19 04:54:34 +0000

    Update papcap-uninstall.sh

--- papcap-uninstall.sh
+++ papcap-uninstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sudo rm -rf /etc/ppp/plugins/
-sudo rm /etc/ppp/pppoe-server-options
-sudo rm -rf /usr/share/doc/rp-pppoe-4.0/
-sudo rm -rf /usr/sbin/pppoe-relay
-sudo rm -rf /usr/sbin/pppoe-sniff
-sudo rm -rf /usr/sbin/pppoe-server
-sudo rm -rf /usr/sbin/pppoe
-sudo rm /usr/share/man/man8/pppoe.8
-sudo rm /usr/share/man/man8/pppoe-server.8
-sudo rm /usr/share/man/man8/pppoe-sniff.8
-sudo rm /usr/share/man/man8/pppoe-relay.8
-sudo rm /usr/local/sbin/rp-pppoe-server.sh
-sudo apt remove ppp-dev
-sudo apt autoremove
+rm -rf /etc/ppp/plugins/
+rm /etc/ppp/pppoe-server-options
+rm -rf /usr/share/doc/rp-pppoe-4.0/
+rm -rf /usr/sbin/pppoe-relay
+rm -rf /usr/sbin/pppoe-sniff
+rm -rf /usr/sbin/pppoe-server
+rm -rf /usr/sbin/pppoe
+rm /usr/share/man/man8/pppoe.8
+rm /usr/share/man/man8/pppoe-server.8
+rm /usr/share/man/man8/pppoe-sniff.8
+rm /usr/share/man/man8/pppoe-relay.8
+rm /usr/local/sbin/rp-pppoe-server.sh
+apt remove ppp-dev
+apt autoremove
